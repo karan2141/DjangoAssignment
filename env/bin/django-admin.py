d497d7f9aafc99723671d0d83a10779901326fe0
karan2141 <kvs2141@gmail.com>
2020-04-24 21:15:00 +0530

    started project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/karanveer/Downloads/new-Assignment/DjangoAssignment/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
